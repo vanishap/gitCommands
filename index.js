c3c65c66ae6fe8b48c01296fa1e9d874a5a3f399
Ashton <asch8216@gmail.com>
2022-03-18 10:41:32 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+function add(num1, num2) {
+  return num1 + num2;
+}
+
+console.log(add(2, 3));
+
+if (true) {
+  console.log('Technosoft is the best school in town');
+}
---
+++ obj/index.js
@@ -0,0 +1,41 @@
+const tv = {
+  brand: 'Samsung',
+  size: '90in',
+  price: 1499,
+  resolution: '4k',
+  onSale: true,
+  discount: 550,
+  googleCompatible: true,
+  reviews: [{
+    reviewer: 'chantell',
+    stars: 5,
+    review: 'best tv ever',
+    photos: ['link to where phots being stored'],
+  }],
+  posted: 'somedate',
+};
+
+const prof = {
+  name: 'kathleen smith',
+  university: 'Duke',
+  dept: 'Biology',
+  levelOfDifficulty: 3,
+  ratings: [{
+    tag: ['respected'],
+  }],
+  rating: 4.8,
+  ratingDistribution: {
+    awesome: 4,
+    great: 3,
+    good: 0,
+    ok: -1,
+    awful: -2,
+  },
+  similarProf: [{
+    name: 'Brian S',
+    rating: 5,
+  }],
+
+};
+
+const tags = { respected: 5, participation: 25 };
