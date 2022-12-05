58367577e07d2d6e06bb35e340da67dc0d3d5ae9
vanishap <vanishareddyp@gmail.com>
2022-12-05 08:10:57 -0800

    new function and console.log added

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 function add(num1, num2) {
   return num1 + num2;
 }
-
+function subtract(a,b){
+  return a-b;
+}
 console.log(add(2, 3));
+console.log(subtract(10,5));
 
 if (true) {
   console.log('Technosoft is the best school in town');
